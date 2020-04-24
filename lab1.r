f20671510819b51c511c8f6329788ae8c22dcc3e
R. Paul Wiegand <rpwiegand@gmail.com>
2020-04-24 09:30:22 -0400

    Added initial files

---
+++ lab1.r
@@ -0,0 +1,16 @@
+# Please use this source file to complete the lab1 assignment.  
+# To submit, commit and push your source file.
+#
+# There is information in our M&S Bootcamp persistent WebCourse
+# about using Git
+#   https://webcourses.ucf.edu/courses/1249560
+#
+# You can use git directly from RStudio if you want!  Here is more
+# information about that:
+#   https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
+#
+# You are *not permitted* to share your code with others for this 
+# assignment.
+#
+
+#  vvvvv Put your code down here vvvvvv
